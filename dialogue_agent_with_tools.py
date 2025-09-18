38509a1b0eb4206c0ecea0baa3fae5e660554ce6
Sarmisstha <sarmissthask@gmail.com>
2025-09-18 20:24:17 +0530

    Delete dialogue_agent_with_tools.py

--- dialogue_agent_with_tools.py
+++
@@ -1,9 +0,0 @@
-
-from xagent import XAgent
-
-class DialogueAgentWithTools:
-    def __init__(self, tools):
-        self.agent = XAgent(tools=tools)
-
-    def converse(self, message):
-        return self.agent.run(message)
